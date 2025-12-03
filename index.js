c32d04f0b61c066381aa51060bb6382757b9bf9f
acodein <ayushupadhyay373@gmail.com>
2025-12-04 00:34:48 +0530

    fix:begin the module process

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const cells = document.querySelectorAll(".cell");
-const status = document.querySelector("#statusText");
-const restartBtn = document.querySelector("#restartBtn");
 
-const winConditions = [
+// later if needed divide the board function into multiple small functions.
+
+const Result = (function(){
+
+    const winConditions = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -12,11 +13,25 @@ const winConditions = [
     [0, 4, 8],
     [2, 4, 6]
 ];
+console.log(winConditions);
+return {winConditions};
 
-let options = ["", "", "", "", "", "", "", "", ""];
+})();
+
+
+const Board =function(){
+
+const win = Result;
+const cells = document.querySelectorAll(".cell");
+const status = document.querySelector("#statusText");
+const restartBtn = document.querySelector("#restartBtn");
+
+
 let currentPlayer = "X";
 let running = false;
 
+let options = ["", "", "", "", "", "", "", "", ""];
+// try to initialize the game using button.
 initializeGame();
 
 function initializeGame(){
@@ -47,11 +62,20 @@ function changePlayer(){
     statusText.textContent = `${currentPlayer}'s turn`;
 }
 
-function checkWinner(){
+function restartGame(){
+    currentPlayer = "X";
+    options = ["", "", "", "", "", "", "", "", ""];
+
+    statusText.textContent = `${currentPlayer}'s turn`;
+    cells.forEach(cell=> cell.textContent = '');
+    running = true;
+}
+
+const checkWinner = ()=>{
     let roundWon = false;
 
-    for(let i=0; i < winConditions.length; i++){
-        const condition = winConditions[i];
+    for(let i=0; i < win.winConditions.length; i++){
+        const condition = win.winConditions[i];
         const cellA = options[condition[0]];
         const cellB = options[condition[1]];
         const cellC = options[condition[2]];
@@ -75,15 +99,11 @@ function checkWinner(){
     }
 }
 
-function restartGame(){
-    currentPlayer = "X";
-    options = ["", "", "", "", "", "", "", "", ""];
 
-    statusText.textContent = `${currentPlayer}'s turn`;
-    cells.forEach(cell=> cell.textContent = '');
-    running = true;
-}
+return{changePlayer,updateCell,cellClicked,initializeGame}
+};
 
+const gaem = Board();
 
 
 
